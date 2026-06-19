9050d6d111b610a16ca4985a12cb824ca55a2ad0
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2026-06-19 11:09:02 +0300

    add drive

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,5 +1,13 @@
 import Cookies from "js-cookie";
 
+const cookieOptions: Cookies.CookieAttributes = {
+  sameSite: "strict",
+  secure: window.location.protocol === "https:",
+  ...(window.location.hostname.endsWith("i3alumba.ru")
+    ? { domain: ".i3alumba.ru" }
+    : {}),
+};
+
 export const getAccessToken = (): string | undefined =>
   Cookies.get("access_token");
 
@@ -7,11 +15,11 @@ export const getRefreshToken = (): string | undefined =>
   Cookies.get("refresh_token");
 
 export const setTokens = (access: string, refresh: string) => {
-  Cookies.set("access_token", access, { sameSite: "strict" });
-  Cookies.set("refresh_token", refresh, { sameSite: "strict" });
+  Cookies.set("access_token", access, cookieOptions);
+  Cookies.set("refresh_token", refresh, cookieOptions);
 };
 
 export const clearTokens = () => {
-  Cookies.remove("access_token");
-  Cookies.remove("refresh_token");
+  Cookies.remove("access_token", cookieOptions);
+  Cookies.remove("refresh_token", cookieOptions);
 };
